b2f75b4ff9c4d3c02857117e86b1cb38285e6135
supakorn emchananon <supakorn.emch@gmail.com>
2024-06-26 11:30:38 +0700

    refactor: Use link in app py instead text file

--- app.py
+++ app.py
@@ -1,61 +1,69 @@
-import requests
-from pytube import YouTube
-import os
 from pydub import AudioSegment
-import re
-import urllib.parse
+from pytube import YouTube
 from time import sleep
+from concurrent.futures.thread import ThreadPoolExecutor
+from concurrent.futures import Future
 
-# Function to remove non-alphabet characters from the title.
-def remove_non_alpha(s):
-    return re.sub(r"[^a-zA-Z\s]", '', s).replace(' ', '-')
+import os
+import re
+import requests
+import shutil
+import urllib.parse
+import tqdm
 
-# Validating input url
-try:
-    video_url = input("Paste YouTube URL: ")
-    result = urllib.parse.urlparse(video_url)
 
-    # Check if the URL is valid and belongs to YouTube.com
-    if result.scheme == "https" and result.netloc == "www.youtube.com" or "youtu.be" in video_url:
-        print("URL check... PASS")
-        try:
+def download_mp3_youtube(link_url: str):
+
+    # Validating input url
+    try:
+        video_url = link_url
+        result = urllib.parse.urlparse(video_url)
+
+        # Check if the URL is valid and belongs to YouTube.com
+        if result.scheme == "https" and result.netloc == "www.youtube.com" or "youtu.be" in video_url:
             yt = YouTube(video_url)
-        except:
-            print("Failed to read url. Retrying...")
-            sleep(2)
-            yt = YouTube(video_url)
-        raw_title = yt.title
-        title = remove_non_alpha(raw_title)
+            title = yt.title
 
-        # Send a request to the URL and check if the video is available
-        response = requests.get(video_url)
-        if "Video unavailable" in response.text:
-            print("Video is not available on YouTube")
-            raise ValueError("Video is not available on YouTube")
+            # Send a request to the URL and check if the video is available
+            response = requests.get(video_url)
+            if "Video unavailable" in response.text:
+                raise ValueError("Video is not available on YouTube")
+            else:
+                sleep(1)
+                # Downloading the audio stream
+                audio_stream = yt.streams.filter(only_audio=True).first()
+                audio_file = os.path.join(os.getcwd(), f"temp/{title}.mp4")
+                audio_stream.download(output_path=os.getcwd(), filename=f"temp/{title}.mp4")
+
+                # Convert the downloaded audio to MP3
+                audio: AudioSegment = AudioSegment.from_file(os.path.abspath(audio_file), format="mp4")
+                mp3_file = os.path.join(os.getcwd(), f"temp/{title}.mp3")
+                audio.export(os.path.abspath(mp3_file), format="mp3", bitrate="320k")
+
+                # Clean up - remove the original MP4 audio file
+                os.remove(audio_file)
+                shutil.move(mp3_file, os.path.join(os.getcwd(), "temp", os.path.basename(mp3_file)))
         else:
-            print(f"Title: {title}")
-            print("Gathering available streams...")
-            sleep(2)
+            raise ValueError("Invalid URL or URL does not belong to youtube.com")
 
-            # Downloading the audio stream
-            audio_stream = yt.streams.filter(only_audio=True).first()
-            audio_file = os.path.join(os.getcwd(), f"{title}.mp4")
-            audio_stream.download(output_path=os.getcwd(), filename=f"{title}.mp4")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-            # Convert the downloaded audio to MP3
-            audio = AudioSegment.from_file(os.path.abspath(audio_file), format='mp4')
-            mp3_file = os.path.join(os.getcwd(), f"{title}.mp3")
-            audio.export(os.path.abspath(mp3_file), format='mp3', bitrate='320k')
 
-            # Clean up - remove the original MP4 audio file
-            os.remove(audio_file)
+if __name__ == "__main__":
+    os.makedirs("temp", exist_ok=True)
+    youtubelist = ["YOUR_LINKS"]
 
-            print("Audio conversion completed")
-            print(f"MP3 file saved: {mp3_file}")
+    futures: list[Future] = []
+    results = []
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        for url_txt in youtubelist:
+            futures.append(executor.submit(download_mp3_youtube, url_txt))
 
-    else:
-        print("URL is not valid or does not belong to youtube.com")
-        raise ValueError("Invalid URL or URL does not belong to youtube.com")
-
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+        for f in tqdm.tqdm(futures, total=len(futures)):
+            try:
+                result = f.result()
+                results.append(result)
+            except Exception as e:
+                print(f"Download failed for {f}: {e}")
+                results.append(None)
